0d7326b24d2ed5f3e22fe1ab89c7d06ae548508e
RY4N <ryan.is.nomore7@gmail.com>
2025-05-26 04:48:08 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ module.exports = {
   token: process.env.token || "",
   channelId: process.env.channelId || "1359044851135549573",
 
-  webMonitor: true, // Set to false if you don't want to use web-monitor
-  expressPort: process.env.expressPort || 6375, // Port for web monitor
+  webMonitor: true,
+  expressPort: process.env.expressPort || 6375,
 
   nodes: [
-     {
+    {
       host: "us2.sanode.xyz",
       password: "discord.gg/W2GheK3F9m",
       port: 5022,
@@ -14,8 +14,9 @@ module.exports = {
       secure: false,
       reconnectTimeout: 300000,
       reconnectTries: 100,
+      version: 3
     },
-     {
+    {
       host: "ryan-lavalink-v4.elixirno.de",
       password: "discord.gg/W2GheK3F9m",
       port: 25682,
@@ -23,17 +24,9 @@ module.exports = {
       secure: false,
       reconnectTimeout: 300000,
       reconnectTries: 100,
+      version: 4
     },
     {
-      host: "ryan-lavalink-v4.elixirno.de",
-      password: "discord.gg/W2GheK3F9m",
-      port: 25567,
-      identifier: "Synthix Host",
-      secure: false,
-      reconnectTimeout: 300000,
-      reconnectTries: 100,
-    },
-     {
       host: "79.110.236.32",
       password: "discord.gg/W2GheK3F9m",
       port: 9033,
@@ -41,6 +34,7 @@ module.exports = {
       secure: false,
       reconnectTimeout: 300000,
       reconnectTries: 100,
-    },
-  ],
+      version: 4
+    }
+  ]
 };
